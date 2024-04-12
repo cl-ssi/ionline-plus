7a28b8cda90300de9327658944e45f30658583ac
AquaroTorres <alvarotorres@gmail.com>
2024-04-11 23:11:55 -0400

    iOnline logo widget

---
+++ app/Filament/Widgets/IonlineLogo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class IonlineLogo extends Widget
+{
+    protected static string $view = 'filament.widgets.ionline-logo';
+}

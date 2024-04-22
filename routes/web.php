d25e19af1556701c1e046bbe93443ffee83da122
AquaroTorres <alvarotorres@gmail.com>
2024-04-21 22:15:29 -0400

    Admin panel to /

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\PdfController;
 use Illuminate\Support\Facades\Route;
 
 /* Redirección de la raíz a la página de inicio de filament */
-Route::redirect('/', '/admin',301);
+// Route::redirect('/', '/admin',301);
 
-Route::get('/login', function () { return redirect('/admin'); })->name('login');
+// Route::get('/login', function () { return redirect('/admin'); })->name('login');
 
 Route::get('/ionline/login/{id}', [IonlineController::class,'login']);
 Route::get('/ionline/return-to-ionline', [IonlineController::class,'returnToIonline'])->name('return-to-ionline');

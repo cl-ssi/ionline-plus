e01cb93717f7663f5e053568e0da5b34b9e1dd27
AquaroTorres <alvarotorres@gmail.com>
2024-05-08 11:53:46 -0400

    News widget controles ocultos cuando no hay noticias

--- resources/views/filament/widgets/news-widget.blade.php
+++ resources/views/filament/widgets/news-widget.blade.php
@@ -1,13 +1,29 @@
 <x-filament-widgets::widget>
     <div class="flex justify-between p-1">
-        <button wire:click="prev">Anterior</button>
-        <button wire:click="next">Siguiente</button>
+        <div>
+            @if ($this->currentIndex > 0)
+                <button wire:click="prev">Anterior</button>
+            @else
+                <div class="invisible">
+                    <button disabled>Anterior</button>
+                </div>
+            @endif
+        </div>
+        
+        <div>
+            @if ($this->currentIndex < $this->news->count() - 1)
+                <button wire:click="next">Siguiente</button>
+            @else
+                <div class="invisible">
+                    <button disabled>Siguiente</button>
+                </div>
+            @endif
+        </div>
     </div>
     <x-filament::section 
         class="p-1 bg-cover bg-center rounded-lg" 
-        style="background-image: url('{{ Storage::disk('gcs')->url($this->getCurrentNews()->image) }}'); height: 420px;"
-        width>
+        style="height: 420px; background-image: url('{{ Storage::disk('gcs')->url($this->getCurrentNews()->image) }}');">
     </x-filament::section>
-    <p class="p-1 small">{{ $this->getCurrentNews()->publication_date_at }} - {{ $this->getCurrentNews()->title }}</p>
-    <p>{{ $this->getCurrentNews()->body }}</p>
+    <p class="p-1">{{ $this->getCurrentNews()->publication_date_at }} - {{ $this->getCurrentNews()->title }}</p>
+    <p class="p-1">{{ $this->getCurrentNews()->body }}</p>
 </x-filament-widgets::widget>

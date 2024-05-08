c5fb5d5cf9444e5cd18689c77f088c55e94aa2ed
AquaroTorres <alvarotorres@gmail.com>
2024-05-08 11:30:21 -0400

    News Widget

--- app/Filament/Widgets/NewsWidget.php
+++ app/Filament/Widgets/NewsWidget.php
@@ -4,7 +4,6 @@ namespace App\Filament\Widgets;
 
 use App\Models\News;
 use Filament\Widgets\Widget;
-use Illuminate\Support\Facades\Storage;
 
 class NewsWidget extends Widget
 {
@@ -12,14 +11,30 @@ class NewsWidget extends Widget
 
     protected int | string | array $columnSpan = 2;
 
-    public $newsItems;
+    public $news;
+    public $currentIndex = 0;
 
     public function mount(): void
     {
-        $this->newsItems = News::orderBy('id','asc')->get()->map(function ($news) {
-            return [
-                'url' => Storage::disk('gcs')->url($news->image)
-            ];
-        });
+        $this->news = News::orderBy('id', 'desc')->get();
+    }
+
+    public function next()
+    {
+        if ($this->currentIndex < $this->news->count() - 1) {
+            $this->currentIndex++;
+        }
+    }
+
+    public function prev()
+    {
+        if ($this->currentIndex > 0) {
+            $this->currentIndex--;
+        }
+    }
+
+    public function getCurrentNews()
+    {
+        return $this->news[$this->currentIndex];
     }
 }
--- resources/views/filament/widgets/news-widget.blade.php
+++ resources/views/filament/widgets/news-widget.blade.php
@@ -1,31 +1,13 @@
 <x-filament-widgets::widget>
-    <x-filament::section>
-        <div class="p-0" x-data="{ activeSlide: 0, slides: @js($newsItems), next() { this.activeSlide = (this.activeSlide + 1) % this.slides.length; }, previous() { this.activeSlide = (this.activeSlide + this.slides.length - 1) % this.slides.length; } }">
-            <div class="flex overflow-hidden relative">
-                <template x-for="(slide, index) in slides" :key="index">
-                    <img :src="slide.url" class="w-full h-64 object-cover transition-transform duration-500 ease-in-out" x-show="activeSlide === index" style="display: none;">
-                </template>
-            </div>
-            <div class="flex justify-center space-x-2 mt-0">
-                <template x-for="(slide, index) in slides" :key="index">
-                    <button
-                        class="w-2 h-2 bg-gray-400 rounded-full"
-                        :class="{ 'bg-blue-500': activeSlide === index }"
-                        @click="activeSlide = index"></button>
-                </template>
-            </div>
-            <!-- Navigation buttons -->
-            <div class="absolute inset-y-0 left-0 flex items-center z-10">
-                <button @click="previous()" class="bg-white bg-opacity-75 p-2 rounded-full text-black">
-                    &#9664; <!-- Left arrow -->
-                </button>
-            </div>
-            <div class="absolute inset-y-0 right-0 flex items-center z-10">
-                <button @click="next()" class="bg-white bg-opacity-75 p-5 rounded-full text-black">
-                    &#9654; <!-- Right arrow -->
-                </button>
-            </div>
-        
-        </div>
+    <div class="flex justify-between p-1">
+        <button wire:click="prev">Anterior</button>
+        <button wire:click="next">Siguiente</button>
+    </div>
+    <x-filament::section 
+        class="p-1 bg-cover bg-center rounded-lg" 
+        style="background-image: url('{{ Storage::disk('gcs')->url($this->getCurrentNews()->image) }}'); height: 420px;"
+        width>
     </x-filament::section>
+    <p class="p-1 small">{{ $this->getCurrentNews()->publication_date_at }} - {{ $this->getCurrentNews()->title }}</p>
+    <p>{{ $this->getCurrentNews()->subtitle }}</p>
 </x-filament-widgets::widget>

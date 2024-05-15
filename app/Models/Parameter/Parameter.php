e3b8f684185d14a51aca9697fdea1e12fc2f4ef1
AquaroTorres <alvarotorres@gmail.com>
2024-05-15 13:06:49 -0400

    Parameter get function

--- app/Models/Parameter/Parameter.php
+++ app/Models/Parameter/Parameter.php
@@ -30,4 +30,31 @@ class Parameter extends Model
     {
         return $this->belongsTo(Establishment::class);
     }
+
+    /** Requiere revisión para que no se caiga en caso de que no exista el parámetro */
+    // public static function get($module, $parameter, $establishment_id = null)
+    // {
+    //     $query = Parameter::where('module', $module);
+    //         // ->where('parameter', $parameter);
+    //     if(is_array($parameter)){
+    //         $query->whereIn('parameter', $parameter);
+    //     }else{
+    //         $query->where('parameter', $parameter);
+    //     }
+
+    //     if ($establishment_id !== null) {
+    //         if(is_array($establishment_id)){
+    //             $query->whereIn('establishment_id', $establishment_id);
+    //         }else{
+    //             $query->where('establishment_id', $establishment_id);
+    //         }
+    //     }
+
+    //     /** 
+    //      * @alupa: 2021-07-20
+    //      * $query->first()->value da error si el parámetro no existe, debería retornar null.
+    //      */
+    //     if(isset($parameter)) return is_array($parameter) || is_array($establishment_id) ? $query->pluck('value')->toArray() : ($query->first()->value ?? null);
+    //     else return null;
+    // }
 }

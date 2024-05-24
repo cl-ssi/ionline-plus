acfb8096c3af7199e8e38948dc59955b02e779c3
AquaroTorres <alvarotorres@gmail.com>
2024-05-24 15:59:08 -0400

    Test Clave Unica

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -1,42 +1,53 @@
 <?php
 
 namespace App\Http\Controllers;
- 
+
 use App\Models\User;
 use Laravel\Socialite\Facades\Socialite;
- 
+
 class SocialiteController extends Controller
 {
     public function redirect(string $provider)
     {
         return Socialite::driver($provider)->redirect();
     }
- 
+
     public function callback(string $provider)
     {
-        $response = Socialite::driver($provider)->user();
- 
-        $user = User::firstWhere(['id' => $response->getId()]);
- 
-        if ($user) {
-            logger()->info('Entro en el IF', [
-                'response' => $response,
-                'user' => $user,
-            ]);
-            // $user->update([$provider . '_id' => $response->getId()]);
-        } else {
-            logger()->info('No encontró el usuario en la BD');
-            // $user = User::create([
-            //     $provider . '_id' => $response->getId(),
-            //     'name'            => $response->getName(),
-            //     'email'           => $response->getEmail(),
-            //     'password'        => '',
-            // ]);
+        try {
+            $response = Socialite::driver($provider)
+                ->scopes(['openid', 'run','name'])
+                ->user();
+
+            $user = User::firstWhere(['id' => $response->getId()]);
+
+            // dd($user);
+
+            if ( $user ) {
+                logger()->info('Entro en el IF', [
+                    'response' => $response,
+                    'user'     => $user,
+                ]);
+                // $user->update([$provider . '_id' => $response->getId()]);
+            } else {
+                logger()->info('No encontró el usuario en la BD');
+                // $user = User::create([
+                //     $provider . '_id' => $response->getId(),
+                //     'name'            => $response->getName(),
+                //     'email'           => $response->getEmail(),
+                //     'password'        => '',
+                // ]);
+            }
+
+            auth()->login($user);
+
+            return redirect()->intended(route('filament.admin.pages.dashboard'));
+
+        } catch (\Laravel\Socialite\Two\InvalidStateException $e) {
+            abort(403, 'Invalid state exception: ' . $e->getMessage());
+        } catch (\Exception $e) {
+            abort(403, 'General exception: ' . $e->getMessage());
         }
- 
-        auth()->login($user);
- 
-        return redirect()->intended(route('filament.admin.pages.dashboard'));
     }
- 
+
 }

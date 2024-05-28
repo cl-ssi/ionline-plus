bc291dd0fc8abfadfc98a8230f19b6158e420f50
AquaroTorres <alvarotorres@gmail.com>
2024-05-28 16:08:16 -0400

    Location y place policy

--- app/Policies/Parameter/PlacePolicy.php
+++ app/Policies/Parameter/PlacePolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies\Parameter;
 
-use App\Models\Parameter\Location;
+use App\Models\Parameter\Place;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class LocationPolicy
+class PlacePolicy
 {
     /**
      * Perform pre-authorization checks.
@@ -33,7 +33,7 @@ class LocationPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Location $location): bool
+    public function view(User $user, Place $place): bool
     {
         return false;
     }
@@ -49,7 +49,7 @@ class LocationPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Location $location): bool
+    public function update(User $user, Place $place): bool
     {
         return false;
     }
@@ -57,7 +57,7 @@ class LocationPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Location $location): bool
+    public function delete(User $user, Place $place): bool
     {
         return false;
     }
@@ -65,7 +65,7 @@ class LocationPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Location $location): bool
+    public function restore(User $user, Place $place): bool
     {
         return false;
     }
@@ -73,7 +73,7 @@ class LocationPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Location $location): bool
+    public function forceDelete(User $user, Place $place): bool
     {
         return false;
     }

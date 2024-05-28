d324afc4496f26900a736f0f3c33b85a52feb4d7
AquaroTorres <alvarotorres@gmail.com>
2024-05-28 12:44:58 -0400

    test clave unica

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -11,9 +11,7 @@ class SocialiteController extends Controller
 {
     public function redirect(string $provider)
     {
-        return Socialite::driver($provider)
-            ->setScopes(['openid', 'run','name'])
-            ->redirect();
+        return Socialite::driver($provider)->redirect();
     }
 
     public function callback(string $provider)
@@ -30,9 +28,15 @@ class SocialiteController extends Controller
             } else {
                 // request()->session()->regenerate();
                 // cerrar sesion de CU llamando a una url con HTTP
+                // session()->invalidate();
+                // session()->regenerateToken();
+                session(['userNotFound' => true ]);
+                // session()->flash('msg', 'No existe el usuario en el sistema.');
+                // $redirect = route('filament.admin.auth.login');
+                return redirect()->away('https://accounts.claveunica.gob.cl/api/v1/accounts/app/logout?redirect='.env('APP_URL').'/auth/claveunica/logout');
                 // Http::get('https://accounts.claveunica.gob.cl/api/v1/accounts/app/logout?redirect='.$logout_uri);
-                return redirect()->route('filament.admin.auth.login')
-                    ->withErrors(['msg' => 'No existe el usuario en el sistema.']);
+                // return redirect()->route('filament.admin.auth.login')
+                //     ->withErrors(['msg' => 'No existe el usuario en el sistema.']);
             }
         // } catch (\Laravel\Socialite\Two\InvalidStateException $e) {
         //     request()->session()->regenerate();
@@ -54,6 +58,7 @@ class SocialiteController extends Controller
         switch($provider) {
             case 'claveunica':
                 if(env('APP_ENV') == 'local') {
+                    // si el referer es local, redireccionar a la pagina de inicio
                     return redirect()->route('socialite.logout', ['provider' => 'local']);
                 }
                 else {
@@ -61,15 +66,21 @@ class SocialiteController extends Controller
                     if(request()->headers->get('referer') == 'https://accounts.claveunica.gob.cl/') {
                         return redirect()->route('socialite.logout', ['provider' => 'local']);
                     }
-                    // si el referer es local, cerrar sesion de CU y redireccionar a la pagina de inicio
                     else {
+                        dd(request()->headers->get('referer'));
                         return redirect()->away('https://accounts.claveunica.gob.cl/api/v1/accounts/app/logout?redirect='.env('APP_URL').'/auth/claveunica/logout');
                     }
                 }
             case 'local':
+                if( session()->has('userNotFound') ) {
+                    $userNotFound = session('userNotFound');
+                }
                 Filament::auth()->logout();
                 session()->invalidate();
                 session()->regenerateToken();
+                if( $userNotFound ) {
+                    session()->flash('msg', 'No existe el usuario en el sistema.');
+                }
                 return redirect()->route('filament.admin.auth.login');
             case 'redirect':
                 return redirect()->route('filament.admin.auth.login');
